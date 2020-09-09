afad617605aa8c14960260deecd37594e2227c9a
linux_china <libing.chen@gmail.com>
2020-09-09 00:28:15 -0700

    add test methods

--- src/test/java/io/r2dbc/mysqlx/MysqlOperationsTest.java
+++ src/test/java/io/r2dbc/mysqlx/MysqlOperationsTest.java
@@ -6,18 +6,32 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class MysqlOperationsTest extends MysqlxBaseTest {
     @Test
     public void testQuery() throws Exception {
-        SqlStatement statement = mysqlSession.sql("select * from account");
+        List<Object> binding = new ArrayList<>();
+        binding.add(1);
+        SqlStatement statement = mysqlSession.sql("select * from account where id = ?").bind(binding);
         CompletableFuture<SqlResult> future = statement.executeAsync();
         Mono.fromFuture(future)
-                .flatMapMany(rows -> Flux.fromIterable(rows.fetchAll()))
+                .flatMapMany(sqlResult -> Flux.fromIterable(sqlResult.fetchAll()))
                 .subscribe(row -> {
                     System.out.println(row.getString("nick"));
                 });
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        SqlStatement statement = mysqlSession.sql("insert into people(nick, created_at) values('good2',now())");
+        CompletableFuture<SqlResult> future = statement.executeAsync();
+        Mono.fromFuture(future)
+                .map(SqlResult::getAutoIncrementValue)
+                .subscribe(System.out::println);
         Thread.sleep(2000);
     }
 }
