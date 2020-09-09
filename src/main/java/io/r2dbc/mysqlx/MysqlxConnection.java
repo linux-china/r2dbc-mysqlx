c7087a37e64c770f2e7a9c676b1ae4007c2a5684
linux_china <libing.chen@gmail.com>
2020-09-09 01:46:15 -0700

    implement transaction

--- src/main/java/io/r2dbc/mysqlx/MysqlxConnection.java
+++ src/main/java/io/r2dbc/mysqlx/MysqlxConnection.java
@@ -4,10 +4,13 @@ import com.mysql.cj.xdevapi.Session;
 import com.mysql.cj.xdevapi.SqlResult;
 import io.r2dbc.spi.*;
 import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoProcessor;
 
-import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -18,17 +21,13 @@ import java.util.concurrent.CompletableFuture;
 public class MysqlxConnection implements Connection {
     private final Session mysqlxSession;
     private ConnectionMetadata connectionMetadata;
-    private Timestamp lastHealthCheck;
+    private LocalDateTime lastHealthCheck;
 
     private final MonoProcessor<Void> onClose = MonoProcessor.create();
 
     public MysqlxConnection(Session mysqlxSession) {
         this.mysqlxSession = mysqlxSession;
         init();
-        // health check with timer
-        /* Flux.interval(Duration.ofSeconds(60)).subscribe(timestamp -> {
-            healthCheck();
-        });*/
     }
 
     private void init() {
@@ -42,24 +41,17 @@ public class MysqlxConnection implements Connection {
     }
 
     private void healthCheck() {
-        CompletableFuture<SqlResult> future = mysqlxSession.sql("select now() as now").executeAsync();
-        Mono.fromFuture(future)
-                .flatMap(sqlResult -> {
-                    return Mono.just(sqlResult.fetchOne());
-                })
-                .doOnError(throwable -> {
-                    this.dispose();
-                })
-                .subscribe(row -> {
-                    this.lastHealthCheck = row.getTimestamp("now");
+        // health check with timer
+        Flux.interval(Duration.ofSeconds(60))
+                .flatMap(timestamp -> this.validate(ValidationDepth.REMOTE))
+                .doOnError(throwable -> this.dispose())
+                .subscribe(result -> {
+                    if (result) {
+                        this.lastHealthCheck = LocalDateTime.now();
+                    } else {
+                        this.dispose();
+                    }
                 });
-
-
-    }
-
-    @Override
-    public Publisher<Void> beginTransaction() {
-        return Mono.empty();
     }
 
     @Override
@@ -77,7 +69,20 @@ public class MysqlxConnection implements Connection {
     }
 
     @Override
+    public Publisher<Void> beginTransaction() {
+        mysqlxSession.startTransaction();
+        return Mono.empty();
+    }
+
+    @Override
     public Publisher<Void> commitTransaction() {
+        mysqlxSession.commit();
+        return Mono.empty();
+    }
+
+    @Override
+    public Publisher<Void> rollbackTransaction() {
+        mysqlxSession.rollback();
         return Mono.empty();
     }
 
@@ -88,6 +93,19 @@ public class MysqlxConnection implements Connection {
 
     @Override
     public Publisher<Void> createSavepoint(String name) {
+        mysqlxSession.setSavepoint(name);
+        return Mono.empty();
+    }
+
+    @Override
+    public Publisher<Void> releaseSavepoint(String name) {
+        mysqlxSession.releaseSavepoint(name);
+        return Mono.empty();
+    }
+
+    @Override
+    public Publisher<Void> rollbackTransactionToSavepoint(String name) {
+        mysqlxSession.rollbackTo(name);
         return Mono.empty();
     }
 
@@ -108,27 +126,12 @@ public class MysqlxConnection implements Connection {
 
     @Override
     public IsolationLevel getTransactionIsolationLevel() {
-        return IsolationLevel.READ_UNCOMMITTED;
-    }
-
-    @Override
-    public Publisher<Void> releaseSavepoint(String name) {
-        return Mono.empty();
-    }
-
-    @Override
-    public Publisher<Void> rollbackTransaction() {
-        return Mono.empty();
-    }
-
-    @Override
-    public Publisher<Void> rollbackTransactionToSavepoint(String name) {
-        return Mono.empty();
+        return IsolationLevel.READ_COMMITTED;
     }
 
     @Override
     public Publisher<Void> setAutoCommit(boolean autoCommit) {
-        return Mono.empty();
+        throw new UnsupportedOperationException("MySQL X Protocol doesn't support auto commit!");
     }
 
     @Override
@@ -138,6 +141,10 @@ public class MysqlxConnection implements Connection {
 
     @Override
     public Publisher<Boolean> validate(ValidationDepth depth) {
-        return Mono.empty();
+        if (depth == ValidationDepth.LOCAL) {
+            return Mono.just(true);
+        }
+        CompletableFuture<SqlResult> future = mysqlxSession.sql("select 1").executeAsync();
+        return Mono.fromFuture(future).map(Iterator::hasNext);
     }
 }
