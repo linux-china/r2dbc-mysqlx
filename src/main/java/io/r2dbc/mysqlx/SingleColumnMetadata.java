9b020f0784a53d481992860614216d5afcf3b500
linux_china <libing.chen@gmail.com>
2020-09-08 23:29:19 -0700

    Single column metadata

---
+++ src/main/java/io/r2dbc/mysqlx/SingleColumnMetadata.java
@@ -0,0 +1,49 @@
+package io.r2dbc.mysqlx;
+
+import io.r2dbc.spi.ColumnMetadata;
+import io.r2dbc.spi.Nullability;
+
+/**
+ * Single column metadata
+ *
+ * @author linux_china
+ */
+public class SingleColumnMetadata implements ColumnMetadata {
+    private String column;
+    private Class<?> javaType;
+
+    public SingleColumnMetadata(String column, Class<?> javaType) {
+        this.column = column;
+        this.javaType = javaType;
+    }
+
+    @Override
+    public Class<?> getJavaType() {
+        return this.javaType;
+    }
+
+    @Override
+    public String getName() {
+        return column;
+    }
+
+    @Override
+    public Object getNativeTypeMetadata() {
+        return this.javaType;
+    }
+
+    @Override
+    public Nullability getNullability() {
+        return Nullability.NON_NULL;
+    }
+
+    @Override
+    public Integer getPrecision() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public Integer getScale() {
+        return null;
+    }
+}
