67031b6a0d2a18e5a27ccf538e17e0d38446766a
linux_china <libing.chen@gmail.com>
2020-09-09 00:22:01 -0700

    add health checker & dispose()

--- src/main/java/io/r2dbc/mysqlx/MysqlxConnection.java
+++ src/main/java/io/r2dbc/mysqlx/MysqlxConnection.java
@@ -1,11 +1,14 @@
 package io.r2dbc.mysqlx;
 
 import com.mysql.cj.xdevapi.Session;
-import com.mysql.cj.xdevapi.SqlStatement;
+import com.mysql.cj.xdevapi.SqlResult;
 import io.r2dbc.spi.*;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoProcessor;
+
+import java.sql.Timestamp;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * MySQL X Protocol R2DBC Connection
@@ -15,20 +18,43 @@ import reactor.core.publisher.MonoProcessor;
 public class MysqlxConnection implements Connection {
     private final Session mysqlxSession;
     private ConnectionMetadata connectionMetadata;
+    private Timestamp lastHealthCheck;
 
     private final MonoProcessor<Void> onClose = MonoProcessor.create();
 
     public MysqlxConnection(Session mysqlxSession) {
         this.mysqlxSession = mysqlxSession;
         init();
+        // health check with timer
+        /* Flux.interval(Duration.ofSeconds(60)).subscribe(timestamp -> {
+            healthCheck();
+        });*/
     }
 
     private void init() {
-        SqlStatement statement = mysqlxSession.sql(" select version() as version");
-        statement.executeAsync().thenAccept(sqlResult -> {
-            String version = sqlResult.fetchOne().getString("version");
-            this.connectionMetadata = new MysqlxConnectionMetadata(version);
-        });
+        mysqlxSession
+                .sql(" select version() as version")
+                .executeAsync()
+                .thenAccept(sqlResult -> {
+                    String version = sqlResult.fetchOne().getString("version");
+                    this.connectionMetadata = new MysqlxConnectionMetadata(version);
+                });
+    }
+
+    private void healthCheck() {
+        CompletableFuture<SqlResult> future = mysqlxSession.sql("select now() as now").executeAsync();
+        Mono.fromFuture(future)
+                .flatMap(sqlResult -> {
+                    return Mono.just(sqlResult.fetchOne());
+                })
+                .doOnError(throwable -> {
+                    this.dispose();
+                })
+                .subscribe(row -> {
+                    this.lastHealthCheck = row.getTimestamp("now");
+                });
+
+
     }
 
     @Override
@@ -42,7 +68,11 @@ public class MysqlxConnection implements Connection {
     }
 
     public void dispose() {
-        mysqlxSession.close();
+        try {
+            mysqlxSession.close();
+        } catch (Exception ignore) {
+
+        }
         this.onClose.onComplete();
     }
 
