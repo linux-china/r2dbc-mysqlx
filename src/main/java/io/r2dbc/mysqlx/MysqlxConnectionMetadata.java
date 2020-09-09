0bf3116aac06d2400146387380ad002151949978
linux_china <libing.chen@gmail.com>
2020-09-08 23:25:31 -0700

    MySQL connection metadata

---
+++ src/main/java/io/r2dbc/mysqlx/MysqlxConnectionMetadata.java
@@ -0,0 +1,26 @@
+package io.r2dbc.mysqlx;
+
+import io.r2dbc.spi.ConnectionMetadata;
+
+/**
+ * MySQL connection metadata
+ *
+ * @author linux_china
+ */
+public class MysqlxConnectionMetadata implements ConnectionMetadata {
+    private String version;
+
+    public MysqlxConnectionMetadata(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String getDatabaseProductName() {
+        return "mysql";
+    }
+
+    @Override
+    public String getDatabaseVersion() {
+        return this.version;
+    }
+}
